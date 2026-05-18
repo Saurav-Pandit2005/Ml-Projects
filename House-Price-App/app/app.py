1f40f4077575f6d83a3dd431b52a3b586dfc0bc9
Saurav Pandit <sauravpandit9691@gmail.com>
2026-05-18 18:45:27 +0530

    change model loading in app.py

--- House-Price-App/app/app.py
+++ House-Price-App/app/app.py
@@ -7,7 +7,7 @@ import pandas as pd
 st.set_page_config(page_title="House Price Predictor", page_icon="🏠", layout="centered")
 
 # Load model
-model = joblib.load("model/model.pkl")
+model = joblib.load("../model/model.pkl")
 
 # Title
 st.title("🏠 House Price Prediction App")
